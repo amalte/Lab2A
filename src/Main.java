0ff6c058618f85e6f506e04d628fe35205244e41
sam123bir <sam.salek@hotmail.com>
2019-11-25 11:12:50 +0100

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CarController carController = new CarController();
+        CarView carView = new CarView("Test", carController);
+    }
+}
+
